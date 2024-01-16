9dcf7a1d7a536a538f4b52997bc124f78aed9bbf
Oladipupo0304 <oladipupoabdultawab2023@gmail.com>
2024-01-16 13:55:28 +0100

    update some changes

--- ContactManager.cs
+++ ContactManager.cs
@@ -6,99 +6,183 @@ namespace BasicContactList
     internal sealed class ContactManager : IContactManager
     {
         public static List<Contact> Contacts = new();
+        //implementing my interface method(AddContact)
         public void AddContact(string name, string phoneNumber, string? email, ContactType contactType)
         {
-            int id = Contacts.Count > 0 ? Contacts.Count + 1 : 1;
-
-            var isContactExist = IsContactExist(phoneNumber);
-
-            if (isContactExist)
+            try
             {
-                Console.WriteLine("Contact already exist!");
-                return;
+                int id = Contacts.Count > 0 ? Contacts.Count + 1 : 1;
+                var isContactExist = IsContactExist(phoneNumber);
+                if (isContactExist)
+                {
+                    Console.WriteLine("Contact already exist!");
+                    return;
+                }
+
+                    var contact = new Contact
+                {
+                    Id = id,
+                    Name = name,
+                    PhoneNumber = phoneNumber,
+                    Email = email,
+                    ContactType = contactType,
+                    CreatedAt = DateTime.Now
+                };
+
+                    Contacts.Add(contact);
+                     Console.WriteLine("Contact added successfully.");
+
+                using (StreamWriter contactList = new ("contactList.txt", true))
+                {
+                    contactList.WriteLine($"Name:  {name}\n" + $"Phone Number: {phoneNumber}\n" + $"Email: {email}\n" + $"Contact Type: {contactType}\n" + $"Created at: {DateTime.Now}\n");
+                    
+                } 
+                
+               
             }
-
-            var contact = new Contact
+            catch (Exception ex)
             {
-                Id = id,
-                Name = name,
-                PhoneNumber = phoneNumber,
-                Email = email,
-                ContactType = contactType,
-                CreatedAt = DateTime.Now
-            };
-
-            Contacts.Add(contact);
-            Console.WriteLine("Contact added successfully.");
+                Console.WriteLine($"An error occurred while adding this  contact: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("");
+            }
         }
 
+        //implementing my interface method(DeleteContact)
         public void DeleteContact(string phoneNumber)
         {
-            var contact = FindContact(phoneNumber);
-
-            if (contact is null)
+           try
             {
-                Console.WriteLine("Unable to delete contact as it does not exist!");
-                return;
-            }
+                
+                var contact = FindContact(phoneNumber);
 
-            Contacts.Remove(contact);
+              
+
+                if (contact is null)
+                {
+                    Console.WriteLine("Unable to delete contact as it does not exist!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("contact deleted successfully");
+                }
+
+                Contacts.Remove(contact);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while deleting a contact: {ex.Message}");
+            }
         }
 
+        //implementing my interface method(FindContact)
         public Contact? FindContact(string phoneNumber)
         {
-            return Contacts.Find(c => c.PhoneNumber == phoneNumber);
+            try
+            {
+                return Contacts.Find(c => c.PhoneNumber == phoneNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while finding a contact: {ex.Message}");
+                return null; 
+            }
         }
 
+        //implementing my interface method(GetContact)
         public void GetContact(string phoneNumber)
         {
-            var contact = FindContact(phoneNumber);
-            
-            if (contact is null)
-            {
-                Console.WriteLine($"Contact with {phoneNumber} not found");
-            }
-            else
-            {
-                Print(contact);
-            }
+          
+           try
+           {
+          
+                var contact = FindContact(phoneNumber);
+                
+                if (contact is null)
+                {
+                    Console.WriteLine($"Contact with {phoneNumber} not found");
+                }
+                else
+                {
+                    Print(contact);
+                }
+          
+           }
+           catch (Exception ex)
+           {
+            Console.WriteLine("an error occur while finding a contact!!:" + ex.Message);
+           }
         }
 
+        //implementing my interface method(GetAllContact)
         public void GetAllContacts()
         {
-            int contactCount = Contacts.Count;
+         try
+         {
+                   int contactCount = Contacts.Count;
 
-            Console.WriteLine("You have " + "contact".ToQuantity(contactCount));
+                Console.WriteLine("You have " + "contact".ToQuantity(contactCount));
 
-            if (contactCount == 0)
-            {
-                Console.WriteLine("There is no contact added yet.");
-                return;
-            }
+                if (contactCount == 0)
+                {
+                    Console.WriteLine("There is no contact added yet.");
+                    return;
+                }
 
-            var table = new ConsoleTable("Id", "Name", "Phone Number", "Email", "Contact Type", "Date Created");
+                var table = new ConsoleTable("Id", "Name", "Phone Number", "Email", "Contact Type", "Date Created");
 
-            foreach (var contact in Contacts)
-            {
-                table.AddRow(contact.Id, contact.Name, contact.PhoneNumber, contact.Email, ((ContactType)contact.ContactType).Humanize(), contact.CreatedAt.Humanize());
-            }
+                foreach (var contact in Contacts)
+                {
+                    table.AddRow(contact.Id, contact.Name, contact.PhoneNumber, contact.Email, ((ContactType)contact.ContactType).Humanize(), contact.CreatedAt.Humanize());
+                }
 
-            table.Write(Format.Alternative);
+                table.Write(Format.Alternative);
+
+              
+
+                using (StreamReader readAllContacts = new ("ContactList.txt"))
+                {
+                    string? contactList = readAllContacts.ReadLine();
+                    while (contactList is not null)
+                    {
+                        Console.WriteLine(contactList);
+                        contactList = readAllContacts.ReadLine();
+                    }
+                }
+         }
+         catch(Exception ex)
+         {
+            Console.WriteLine("an error occur while getting all contact" + ex.Message);
+
+         }
         }
-
+        //implementing my interface method( Updateontact )
         public void UpdateContact(string phoneNumber, string name, string email)
         {
-            var contact = FindContact(phoneNumber);
+           try
+           {
+                 var contact = FindContact(phoneNumber);
 
-            if (contact is null)
-            {
-                Console.WriteLine("Contact does not exist!");
-                return;
-            }
+                if (contact is null)
+                {
+                    Console.WriteLine("Contact does not exist!");
+                    return;
+                }
 
-            contact.Name = name;
-            contact.Email = email;
-            Console.WriteLine("Contact updated successfully.");
+                contact.Name = name;
+                contact.Email = email;
+                Console.WriteLine("Contact updated successfully.");
+
+             }
+             catch(Exception ex)
+             {
+                Console.WriteLine("an error occur when updating contact:" + ex.Message);
+                
+             }
+          
         }
 
         private void Print(Contact contact)
@@ -112,3 +196,10 @@ namespace BasicContactList
         }
     }
 }
+
+
+
+
+
+
+
--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 
 Menu menu = new();
 menu.MyMenu();
+
+
+
+
